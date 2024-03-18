f92667c149e9af426aed9fca10fae8f119c98505
dweller4085 <immbirj@gmail.com>
2024-03-18 21:42:31 +0700

    somewhat abstracted gl workflow

--- src/learngl.cc
+++ src/learngl.cc
@@ -10,8 +10,30 @@
 #include "common.hh"
 
 namespace {
+    constexpr float magic_mouse_sensitivity_constant = 0.0004f;
     
-    constexpr float magic_mouse_sensitivity_constant = 0.0005f;
+    struct t_vertex {
+        vec3 pos;
+        vec2 uv;
+    };
+
+    struct t_simple_cube {
+        static constexpr int vertex_count = 36;
+        
+        static function create(char const * texture_path) -> t_simple_cube;
+        
+        function render() -> void;
+        function set_mvp(mat4 const& mvp) -> void;
+        
+        uint vao;
+        uint shader;
+        uint texture;
+    };
+    
+    namespace cursor {
+        function get_position() -> vec2;
+        function get_delta() -> vec2;
+    };
     
     struct t_camera {
         static constexpr float max_pitch = 0.25f - 1.f / 256.f;
@@ -34,15 +56,11 @@ namespace {
         bool32 can_move;
         
         mat4 view;
-    };
-    
-    struct t_cursor {
-        static function get_position() -> vec2;
-        bool32 is_enabled;
+        mat4 projection;
     };
     
     struct t_app {
-        static function on_key_event(GLFWwindow __in *, int key, int scancode, int action, int mods) -> void;
+        static function on_key_event(GLFWwindow __in * window, int key, int scancode, int action, int mods) -> void;
         
         function init() -> void;
         function update(float dt) -> void;
@@ -50,20 +68,15 @@ namespace {
         function run() -> void;
         function terminate() -> void;
         
-        
         t_camera camera;
-        t_cursor cursor;
         
         GLFWwindow * window;
         int width;
         int height;
         
-        mat4 projection;
+        vec3 background_color;
         
-        // NOTE this is temporary as fuck
-        uint texture;
-        uint shader_program;
-        uint vao;
+        t_simple_cube cube;
     };
     
     inline function fps_view(vec3 position, float pitch, float yaw) -> mat4 {
@@ -76,22 +89,26 @@ namespace {
         return glm::inverse(transform);
     }
     
+    function load_texure(char const * path) -> uint;
+    function create_simple_shader() -> uint;
+    
+    char const * tile_path = "w:\\learngl\\resources\\tile.jpg";
+    
     t_app app;
 }
 
 function ::t_app::on_key_event(GLFWwindow __in * window, int key, int scancode, int action, int mods) -> void {
-    vec2 static last_pos = t_cursor::get_position();
+    vec2 static last_pos = cursor::get_position();
     
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-        if (app.cursor.is_enabled) {
+        if (glfwGetInputMode(window, GLFW_CURSOR) == GLFW_CURSOR_NORMAL) {
             glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
             glfwSetCursorPos(window, last_pos.x, last_pos.y);
         } else {
-            last_pos = t_cursor::get_position();
+            last_pos = cursor::get_position();
             glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
         }
         
-        app.cursor.is_enabled ^= 1;
         app.camera.can_move ^= 1;
     }
 }
@@ -100,7 +117,7 @@ function ::t_app::init() -> void {
     width = 640;
     height = 480;
     
-    cursor.is_enabled = false;
+    background_color = { 0.2f, 0.2f, 0.2f };
     
     camera = {
         .position = { 0.f, 0.f, 0.f },
@@ -112,8 +129,9 @@ function ::t_app::init() -> void {
         .decel_rate = 50.f,
         .sensitivity = 1.f,
         .fov = 90.f,
-        .can_move = true,
-        .view = fps_view({ 0.f, 0.f, 0.f }, 0.f, 0.f),
+        .can_move = false,
+        .view = {},
+        .projection = {},
     };
     
     m_assert(glfwInit());
@@ -136,7 +154,7 @@ function ::t_app::init() -> void {
     
     glfwSetKeyCallback(window, t_app::on_key_event);
     
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    // glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     // let monitor = glfwGetPrimaryMonitor();
     // let mode = glfwGetVideoMode(monitor);
     // glfwSetWindowMonitor(window, monitor, 0, 0, screen_width, screen_height, mode->refreshRate);
@@ -146,11 +164,139 @@ function ::t_app::init() -> void {
     
     glEnable(GL_DEPTH_TEST);
     
-    /////////////// NOTE this is temporary as fuck /////////////////
+    cube = t_simple_cube::create(tile_path);
+}
+
+function ::t_app::update(float dt) -> void {
+    camera.update(dt);
+}
+
+function ::t_app::run() -> void {
+    let then = static_cast<float>(glfwGetTime());
+    let dt = 1.f / 60.f;
     
-    char const * box_texture_path = "w:\\learngl\\resources\\tile.jpg";
+    while (!glfwWindowShouldClose(window)) {
+        update(dt);
+        render();
+        
+        glfwPollEvents();
+        
+        let now = static_cast<float>(glfwGetTime());
+        dt = now - then;
+        then = now;
+    }
+}
+
+function ::t_app::render() -> void {
+    camera.projection = glm::perspective(glm::radians(camera.fov / 2.f), (float) width / (float) height, 0.1f, 100.0f);
     
-    char const * vertex_shader_src = R"(
+    glClearColor(background_color.r, background_color.b, background_color.b, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    cube.set_mvp(camera.projection * camera.view * glm::identity<mat4>());
+    cube.render();
+    
+    glfwSwapBuffers(window);
+}
+
+function ::t_app::terminate() -> void {
+    glfwTerminate();
+}
+
+function ::cursor::get_position() -> vec2 {
+    double x, y;
+    glfwGetCursorPos(app.window, &x, &y);
+    return { static_cast<float>(x), static_cast<float>(y) };
+}
+
+function ::cursor::get_delta() -> vec2 {
+    vec2 static previous = cursor::get_position();
+    vec2 static filter_buffer[2] = {};
+    int static i = 0;
+    
+    let current = cursor::get_position();
+    
+    filter_buffer[i] = current - previous;
+    
+    previous = current;
+    i = (i + 1) % 2;
+    
+    return (filter_buffer[0] + filter_buffer[1]) * 0.5f;
+}
+
+function ::t_camera::update(float dt) -> void {
+    if (can_move) {
+        { // update pitch and yaw angle values
+            let delta = cursor::get_delta() * magic_mouse_sensitivity_constant;
+            delta.x *= sensitivity;
+            delta.y *= 0.86f * sensitivity;
+            
+            yaw = std::fmodf(yaw - delta.x, 1.f);
+            pitch = m_clamp(pitch - delta.y, -max_pitch, max_pitch);
+        }
+        
+        { // update position
+            let rot = glm::identity<mat4>();
+            rot = glm::rotate(rot, yaw * tau, { 0.f, 0.f, 1.f });
+            rot = glm::rotate(rot, pitch * tau, { 1.f, 0.f, 0.f });
+            
+            let front = vec3 { rot * vec4 { 0.f, 1.f, 0.f, 0.f } };
+            let right = vec3 { rot * vec4 { 1.f, 0.f, 0.f, 0.f } };
+            let up = vec3 { 0.f, 0.f, 1.f };
+        
+            let direction = glm::zero<vec3>();
+            
+            direction += front * (float) (glfwGetKey(app.window, GLFW_KEY_W) == GLFW_PRESS);
+            direction -= front * (float) (glfwGetKey(app.window, GLFW_KEY_S) == GLFW_PRESS);
+            direction += right * (float) (glfwGetKey(app.window, GLFW_KEY_D) == GLFW_PRESS);
+            direction -= right * (float) (glfwGetKey(app.window, GLFW_KEY_A) == GLFW_PRESS);
+            direction += up    * (float) (glfwGetKey(app.window, GLFW_KEY_SPACE) == GLFW_PRESS);
+            direction -= up    * (float) (glfwGetKey(app.window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS);
+            
+            let shift = glfwGetKey(app.window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS;
+            
+            if (glm::length(direction) > epsilon) {
+                velocity += glm::normalize(direction) * accel_rate * dt;
+            }
+            
+            let speed = glm::length(velocity);
+            
+            if (speed > epsilon) {
+                velocity = glm::normalize(velocity) * m_clamp(speed - decel_rate * dt, 0.f, max_speed * (shift ? 0.5f : 1.f));
+            }
+            
+            position += velocity * dt;
+        }
+    }
+    
+    view = fps_view(position, pitch, yaw);
+}
+
+
+function ::load_texure(char const * path) -> uint {
+    uint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    int width, height, n_channels;
+    let data = stbi_load(path, &width, &height, &n_channels, 0);
+    m_assert(data);
+    
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    
+    stbi_image_free(data);
+    
+    return texture;
+}
+
+function ::create_simple_shader() -> uint {
+    char static const * vertex_shader_src = R"(
         #version 450 core
         
         layout (location = 0) in vec3 v_pos;
@@ -166,7 +312,7 @@ function ::t_app::init() -> void {
         }
     )";
     
-    char const * fragment_shader_src = R"(
+    char static const * fragment_shader_src = R"(
         #version 450 core
         
         in vec2 f_tex;
@@ -180,7 +326,54 @@ function ::t_app::init() -> void {
         }
     )";
     
-    float vertices[] = {
+    let vertex_shader = [=] {
+        let vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex_shader, 1, &vertex_shader_src, null);
+        glCompileShader(vertex_shader);
+        
+        int ok = 0;
+        glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &ok);
+        m_assert(ok);
+        
+        return vertex_shader;
+    } ();
+    
+    let fragment_shader = [=] {
+        let fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment_shader, 1, &fragment_shader_src, null);
+        glCompileShader(fragment_shader);
+        
+        int ok = 0;
+        glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &ok);
+        m_assert(ok);
+        
+        return fragment_shader;
+    } ();
+    
+    let shader_program = [=] {
+        let shader_program = glCreateProgram();
+        glAttachShader(shader_program, vertex_shader);
+        glAttachShader(shader_program, fragment_shader);
+        glLinkProgram(shader_program);
+        
+        int ok = 0;
+        glGetProgramiv(shader_program, GL_LINK_STATUS, &ok);
+        m_assert(ok);
+        
+        return shader_program;
+    } ();
+    
+    glUseProgram(shader_program);
+    glUniform1i(glGetUniformLocation(shader_program, "f_texture"), 0);
+    
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+    
+    return shader_program;
+}
+
+function t_simple_cube::create(char const * texture_path) -> t_simple_cube {
+    float static vertices[vertex_count * 5] = {
         -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
@@ -224,50 +417,8 @@ function ::t_app::init() -> void {
         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
     };
     
-    shader_program = [=] {
-        let vertex_shader = [=] {
-            let vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-            glShaderSource(vertex_shader, 1, &vertex_shader_src, null);
-            glCompileShader(vertex_shader);
-            
-            int ok = 0;
-            glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &ok);
-            m_assert(ok);
-            
-            return vertex_shader;
-        } ();
-        
-        let fragment_shader = [=] {
-            let fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-            glShaderSource(fragment_shader, 1, &fragment_shader_src, null);
-            glCompileShader(fragment_shader);
-            
-            int ok = 0;
-            glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &ok);
-            m_assert(ok);
-            
-            return fragment_shader;
-        } ();
-        
-        let shader_program = glCreateProgram();
-        
-        glAttachShader(shader_program, vertex_shader);
-        glAttachShader(shader_program, fragment_shader);
-        glLinkProgram(shader_program);
-        
-        int ok = 0;
-        glGetProgramiv(shader_program, GL_LINK_STATUS, &ok);
-        m_assert(ok);
-        
-        glDeleteShader(vertex_shader);
-        glDeleteShader(fragment_shader);
-        
-        return shader_program;
-    } ();
-    
-    
     uint vbo = 0;
-    vao = 0;
+    uint vao = 0;
     
     {
         glGenVertexArrays(1, &vao);
@@ -276,10 +427,11 @@ function ::t_app::init() -> void {
         {
             glGenBuffers(1, &vbo);
             glBindBuffer(GL_ARRAY_BUFFER, vbo);
+            
             glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
             
-            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *) 0);
-            glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *) (1 * sizeof(vec3)));
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(t_vertex), (void *) 0);
+            glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(t_vertex), (void *) (1 * sizeof(vec3)));
             
             glEnableVertexAttribArray(0);
             glEnableVertexAttribArray(1);
@@ -290,154 +442,28 @@ function ::t_app::init() -> void {
         glBindVertexArray(0);
     }
     
-    texture = [=] {
-        uint texture = 0;
-        glGenTextures(1, &texture);
-        glBindTexture(GL_TEXTURE_2D, texture);
-        
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        
-        int width, height, n_channels;
-        let data = stbi_load(box_texture_path, &width, &height, &n_channels, 0);
-        m_assert(data);
-        
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-        
-        glUseProgram(shader_program);
-        glUniform1i(glGetUniformLocation(shader_program, "f_texture"), 0);
-        
-        stbi_image_free(data);
-        
-        return texture;
-    } ();
+    return {
+        .vao = vao,
+        .shader = create_simple_shader(),
+        .texture = load_texure(texture_path),
+    };
 }
 
-function ::t_app::update(float dt) -> void {
-    camera.update(dt);
-}
-
-function ::t_app::run() -> void {
-    let then = static_cast<float>(glfwGetTime());
-    let dt = 1.f / 60.f;
-    
-    while (!glfwWindowShouldClose(window)) {
-        update(dt);
-        render();
-        
-        glfwPollEvents();
-        
-        let now = static_cast<float>(glfwGetTime());
-        dt = now - then;
-        then = now;
-    }
-    
-    terminate();
-}
-
-function ::t_app::render() -> void {
-    projection = glm::perspective(glm::radians(camera.fov / 2.f), (float) width / (float) height, 0.1f, 100.0f);
-    
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    
+function t_simple_cube::render() -> void {
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, texture);
-    
-    glUseProgram(shader_program);
-    
+    glUseProgram(shader);
     glBindVertexArray(vao);
-    
-    let model = glm::identity<mat4>();
-    // model = glm::scale(model, { 0.5f, 0.5f, 0.5f });
-    model = glm::translate(model, { 0.f, 3.f, 0.f });
-    
-    let mvp = projection * camera.view * model;
-    
-    glUniformMatrix4fv(glGetUniformLocation(shader_program, "mvp"), 1, GL_FALSE, glm::value_ptr(mvp));
-    
-    glDrawArrays(GL_TRIANGLES, 0, 36);
-    
-    glfwSwapBuffers(window);
+    glDrawArrays(GL_TRIANGLES, 0, vertex_count);
 }
 
-function ::t_app::terminate() -> void {
-    glfwTerminate();
-}
-
-function ::t_cursor::get_position() -> vec2 {
-    double x, y;
-    glfwGetCursorPos(app.window, &x, &y);
-    return { static_cast<float>(x), static_cast<float>(y) };
-}
-
-function ::t_camera::update(float dt) -> void {
-    if (!can_move) return;
-    
-    { // update pitch and yaw angle values
-        let static previous = t_cursor::get_position();
-        let current = t_cursor::get_position();
-        
-        let delta = (current - previous) * magic_mouse_sensitivity_constant * sensitivity;
-        
-        previous = current;
-        
-        // filter the delta
-        int static i = 0;
-        vec2 static filter_buffer[2] = {};
-        
-        filter_buffer[i] = delta;
-        
-        delta = {};
-        delta += filter_buffer[0];
-        delta += filter_buffer[1];
-        delta /= 2.f;
-        
-        i = (i + 1) % 2;
-        
-        yaw = std::fmodf(yaw - delta.x, 1.f);
-        pitch = m_clamp(pitch - delta.y, -max_pitch, max_pitch);
-    }
-    
-    { // update position
-        let rot = glm::identity<mat4>();
-        rot = glm::rotate(rot, yaw * tau, { 0.f, 0.f, 1.f });
-        rot = glm::rotate(rot, pitch * tau, { 1.f, 0.f, 0.f });
-        
-        let front = vec3 { rot * vec4 { 0.f, 1.f, 0.f, 0.f } };
-        let right = vec3 { rot * vec4 { 1.f, 0.f, 0.f, 0.f } };
-        let up = vec3 { 0.f, 0.f, 1.f };
-    
-        let direction = glm::zero<vec3>();
-        
-        direction += front * (float) (glfwGetKey(app.window, GLFW_KEY_W) == GLFW_PRESS);
-        direction -= front * (float) (glfwGetKey(app.window, GLFW_KEY_S) == GLFW_PRESS);
-        direction += right * (float) (glfwGetKey(app.window, GLFW_KEY_D) == GLFW_PRESS);
-        direction -= right * (float) (glfwGetKey(app.window, GLFW_KEY_A) == GLFW_PRESS);
-        direction += up    * (float) (glfwGetKey(app.window, GLFW_KEY_SPACE) == GLFW_PRESS);
-        direction -= up    * (float) (glfwGetKey(app.window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS);
-        
-        if (glm::length(direction) > epsilon) {
-            velocity += glm::normalize(direction) * accel_rate * dt;
-        }
-        
-        let speed = glm::length(velocity);
-        
-        if (speed > epsilon) {
-            velocity = glm::normalize(velocity) * m_clamp(speed - decel_rate * dt, 0.f, max_speed);
-        }
-        
-        position += velocity * dt;
-    }
-    
-    view = fps_view(position, pitch, yaw);
+function t_simple_cube::set_mvp(mat4 const& mvp) -> void {
+    glUniformMatrix4fv(glGetUniformLocation(shader, "mvp"), 1, GL_FALSE, glm::value_ptr(mvp));
 }
 
 function main(int argc, char ** argv) -> int {
     app.init();
     app.run();
+    app.terminate();
 }
 
--- src/shader.hh
+++
@@ -1,8 +0,0 @@
-#ifndef __learngl_shader__
-#define __learngl_shader___
-
-struct t_shader {
-    
-};
-
-#endif // __learngl_shader__
